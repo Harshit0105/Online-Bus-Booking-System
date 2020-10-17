f634ab2851b51c1025d1eff8f3e413094c3f48ce
Harshit0105 <Harshit010501@gmail.com>
2020-10-18 00:16:54 +0530

    All CRUD methods (BusController)

--- BusBooking/Controllers/BusController.cs
+++ BusBooking/Controllers/BusController.cs
@@ -18,9 +18,55 @@ namespace BusBooking.Controllers
         }
         public IActionResult Index()
         {
-            return View();
+            var model = _busRepo.GetAllBuses();
+            return View(model);
         }
 
+        public IActionResult Details(int id)
+        {
+            Bus bus = _busRepo.GetBus(id);
+            if (bus == null)
+                return RedirectToAction("index");
+            return View(bus);
+        }
+        [HttpGet]
+        public ViewResult Edit(int id)
+        {
+            Bus bus = _busRepo.GetBus(id);
+            Bus newBus = new Bus
+            {
+                Bus_Id = bus.Bus_Id,
+                Souce_City=bus.Souce_City,
+                Destination_City = bus.Destination_City,
+                Number_Of_Seats = bus.Number_Of_Seats,
+                Source_Time = bus.Source_Time,
+                Destination_Time = bus.Destination_Time,
+                Price = bus.Price,
+                Available = bus.Available,
+                Bus_Type = bus.Bus_Type
+            };
+            return View(newBus);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Bus model)
+        {
+            if (ModelState.IsValid)
+            {
+                Bus bus = _busRepo.GetBus(model.Bus_Id);
+                    bus.Souce_City = model.Souce_City;
+                    bus.Destination_City = model.Destination_City;
+                    bus.Number_Of_Seats = model.Number_Of_Seats;
+                    bus.Source_Time = model.Source_Time;
+                    bus.Destination_Time = model.Destination_Time;
+                    bus.Price = model.Price;
+                    bus.Available = model.Available;
+                    bus.Bus_Type = model.Bus_Type;
+                Bus updatedBus = _busRepo.Update(bus);
+                return RedirectToAction();
+            }
+            return View(model);
+        } 
         [HttpGet]
         public ViewResult Add()
         {
@@ -44,9 +90,29 @@ namespace BusBooking.Controllers
                     Bus_Type = model.Bus_Type
                 };
                 _busRepo.Add(newBus);
-                return RedirectToAction();
+                return RedirectToAction("index");
             }
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            Bus bus = _busRepo.GetBus(id);
+            if (bus == null)
+            {
+                return RedirectToAction("index");
+            }
+            return View(bus);
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeleteBus(int id)
+        {
+            Console.WriteLine(id);
+            var bus = _busRepo.GetBus(id);
+            if(bus!=null)
+                _busRepo.Delete(bus.Bus_Id);
+            return RedirectToAction("index");
         }
     }
 }
