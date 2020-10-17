14c511f7f3dc14aeb36170014df159fec8a92f2b
MaitriManiya <maitri8839maniya@gmai.com>
2020-10-17 20:05:40 +0530

    IBusRepository

---
+++ BusBooking/Repository/IBusRepository.cs
@@ -0,0 +1,21 @@
+﻿using BusBooking.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BusBooking.Repository
+{
+    public interface IBusRepository
+    {
+        Bus GetBus(int Id);
+
+        IEnumerable<Bus> GetBuses();
+
+        Bus Add(Bus bus);
+
+        Bus Update(Bus busChanges);
+
+        Bus Delete(int Id);
+    }
+}
